00a9e204df20fb10129b23de619d33fe9cffac11
Oliviaaarst <oliviakristiani0@gmail.com>
2024-09-11 17:50:30 +0700

    Module Product

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//route resource for products
+Route::resource('/products', \App\Http\Controllers\ProductController::class);
